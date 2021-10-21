4b7886a756422db97cc9d8b252a4e074245e3a9a
Bernese-Corgi <jnyng0621@gmail.com>
2021-10-21 17:52:08 +0900

    feat: Add UserSchema's instance methods to set hashed password

--- server/src/model/user.js
+++ server/src/model/user.js
@@ -14,3 +14,13 @@ const UserSchema = new Schema({
     required: true,
   },
 });
+
+/* ---------------------------- instance methods ---------------------------- */
+/**
+ * 비밀번호를 인수로 받아 schema instance의 hashedPassword 값으로 설정
+ * @param {*} password 해싱할 비밀번호
+ */
+UserSchema.methods.setPassword = async function (password) {
+  const hash = await bcrypt.hash(password, 10);
+  this.hashedPassword = hash;
+};
