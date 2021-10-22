345641280add6163736b8b51c45117f32bb0120f
Bernese-Corgi <jnyng0621@gmail.com>
2021-10-22 18:47:40 +0900

    feat: Add post schema in server-side

---
+++ server/src/model/post.js
@@ -0,0 +1,26 @@
+import { model, Schema } from 'mongoose';
+
+const PostSchema = new Schema({
+  publisher: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  // TODO Mindmap의 node 데이터와 관계 설정
+  title: {
+    type: String,
+    required: true,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  tags: [String],
+  publishedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Post = model('Post', PostSchema);
+
+export default Post;
