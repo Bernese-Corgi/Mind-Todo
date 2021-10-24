d612b4823de35ca4fbfbf6c4f13d45350956d062
Bernese-Corgi <jnyng0621@gmail.com>
2021-10-24 23:17:18 +0900

    feat: Add controller to update sepcific post

--- server/src/api/posts/posts.ctrl.js
+++ server/src/api/posts/posts.ctrl.js
@@ -49,13 +49,45 @@ export const write = async (ctx) => {
 };
 
 /* --------------------------- read specific post --------------------------- */
-// GET api/posts/:id
+// GET /api/posts/:id
 export const read = async (ctx) => {
   ctx.body = ctx.state.post;
 };
 
+/* -------------------------- update specific post -------------------------- */
+// PATCH /api/posts/:id
 export const update = async (ctx) => {
-  //
+  // request body 스키마 검증
+  const validatedData = postValidation(ctx.request.body);
+  // request body의 스키마가 검증되지 않으면 에러를 발생시킨다.
+  if (validatedData.error) {
+    ctx.status = 400;
+    ctx.body = validatedData.error;
+    return;
+  }
+
+  // 파라미터에서 id 추출
+  const { id } = ctx.params;
+
+  // 현재 request body 데이터 (수정된 내용) 복사
+  const nextData = { ...ctx.request.body };
+
+  try {
+    // params에서 추출한 id로 post를 찾고, 수정된 데이터를 넣는다.
+    const post = await Post.findByIdAndUpdate(id, nextData, {
+      new: true, // 업데이트된 데이터를 반환
+    }).exec();
+
+    // id로 포스트를 찾을 수 없으면 not found
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    // 수정된 데이터를 응답
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
 export const remove = async (ctx) => {
