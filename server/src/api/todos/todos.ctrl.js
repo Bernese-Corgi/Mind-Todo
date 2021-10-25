9ab21d10bdf576c3bffebd4a6bccbdf1f3887ab6
Bernese-Corgi <jnyng0621@gmail.com>
2021-10-25 15:20:08 +0900

    feat: Add list controller in todos api

--- server/src/api/todos/todos.ctrl.js
+++ server/src/api/todos/todos.ctrl.js
@@ -1,5 +1,18 @@
+import { Todo } from '../../model';
+
+/* -------------------------------- todo list ------------------------------- */
+// GET /api/todos
 export const list = async (ctx) => {
-  // GET /api/todos
+  // TODO 쿼리 파라미터로 투두 필터링 (+ nodeId)
+  // TODO 페이지네이션
+  try {
+    // todos 컬렉션에서 todo list 받아오기
+    const todos = await Todo.find().sort({ _id: -1 });
+    // todos 응답
+    ctx.body = todos;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
 export const write = async (ctx) => {
