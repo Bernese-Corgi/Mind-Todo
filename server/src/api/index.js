bd6741ec4813e69ab35af6a08af42f8bf7bb7594
Bernese-Corgi <jnyng0621@gmail.com>
2021-10-25 15:10:13 +0900

    feat: Add todos routes and Add todos controller draft

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,10 +1,12 @@
 import Router from 'koa-router';
 import auth from './auth';
 import posts from './posts';
+import todos from './todos';
 
 const api = new Router();
 
 api.use('/auth', auth.routes());
 api.use('/posts', posts.routes());
+api.use('/todos', todos.routes());
 
 export default api;
---
+++ server/src/api/todos/todos.ctrl.js
@@ -0,0 +1,15 @@
+export const list = async (ctx) => {
+  // GET /api/todos
+};
+
+export const write = async (ctx) => {
+  // POST /api/todos
+};
+
+export const update = async (ctx) => {
+  // PATCH /api/todos/:id
+};
+
+export const remove = async (ctx) => {
+  // DELETE /api/todos/:id
+};
