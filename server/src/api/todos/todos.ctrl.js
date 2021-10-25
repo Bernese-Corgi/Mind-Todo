b77301fbf7f4e3d66d3bf9b49181ea5355f084c5
Bernese-Corgi <jnyng0621@gmail.com>
2021-10-25 15:35:35 +0900

    feat: Add write todo controller:

--- server/src/api/todos/todos.ctrl.js
+++ server/src/api/todos/todos.ctrl.js
@@ -1,4 +1,5 @@
 import { Todo } from '../../model';
+import { todoValidation } from '../../utils/validation';
 
 /* -------------------------------- todo list ------------------------------- */
 // GET /api/todos
@@ -15,8 +16,34 @@ export const list = async (ctx) => {
   }
 };
 
+/* --------------------------------- todo 작성 -------------------------------- */
+// POST /api/todos
 export const write = async (ctx) => {
-  // POST /api/todos
+  // request body 검증
+  const validatedData = todoValidation(ctx.request.body);
+
+  // request body의 스키마가 검증되지 않으면 bad request 에러
+  if (validatedData.error) {
+    ctx.status = 400;
+    ctx.body = validatedData.error;
+    return;
+  }
+
+  // request body에서 데이터 추출
+  const { content } = ctx.request.body;
+
+  // todo 인스턴스 생성
+  // TODO node id 넣기
+  const todo = new Todo({ content, publisherId: ctx.state.user });
+
+  try {
+    // todo document 저장
+    await todo.save();
+    // todo 응답
+    ctx.body = todo;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
 export const update = async (ctx) => {
