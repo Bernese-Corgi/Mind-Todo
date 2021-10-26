21d7e3f909e01962f2c3af19c2c8e04961672e2f
Bernese-Corgi <jnyng0621@gmail.com>
2021-10-27 04:32:26 +0900

    fix: Apply refactoring middlewares in post routes

--- server/src/api/posts/index.js
+++ server/src/api/posts/index.js
@@ -1,28 +1,30 @@
 import Router from 'koa-router';
 import * as postsCtrl from './posts.ctrl';
-import * as postUtils from '../../utils/postUtils';
+import { getById } from '../../utils';
+import * as checkUser from '../../utils/checkUser';
+import { Post } from '../../model';
 
 const posts = new Router();
 
 posts.get('/', postsCtrl.list);
-posts.post('/', postUtils.checkLoggedIn, postsCtrl.write);
+posts.post('/', checkUser.checkLoggedIn, postsCtrl.write);
 
 const post = new Router();
 
 post.get('/', postsCtrl.read);
 post.patch(
   '/',
-  postUtils.checkLoggedIn,
-  postUtils.checkOwnPost,
+  checkUser.checkLoggedIn,
+  checkUser.checkOwnPost(Post),
   postsCtrl.update,
 );
 post.delete(
   '/',
-  postUtils.checkLoggedIn,
-  postUtils.checkOwnPost,
+  checkUser.checkLoggedIn,
+  checkUser.checkOwnPost(Post),
   postsCtrl.remove,
 );
 
-posts.use('/:id', postUtils.getPostById, post.routes());
+posts.use('/:id', getById(Post), post.routes());
 
 export default posts;
