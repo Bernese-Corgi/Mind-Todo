1bb08d27dd1e09a787379b4abe3cdd54b1a084fe
Bernese-Corgi <jnyng0621@gmail.com>
2021-10-27 03:36:10 +0900

    feat: Add mindmaps routes in server

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -2,11 +2,13 @@ import Router from 'koa-router';
 import auth from './auth';
 import posts from './posts';
 import todos from './todos';
+import mindmaps from './mindmaps';
 
 const api = new Router();
 
 api.use('/auth', auth.routes());
 api.use('/posts', posts.routes());
 api.use('/todos', todos.routes());
+api.use('/mindmaps', mindmaps.routes());
 
 export default api;
---
+++ server/src/api/mindmaps/mindmaps.ctrl.js
@@ -0,0 +1,53 @@
+/* ------------------------------- 마인드맵 목록 조회 ------------------------------- */
+// GET /api/mindmaps
+export const listMindmap = async (ctx) => {
+  //
+};
+
+/* --------------------------------- 마인드맵 작성 -------------------------------- */
+// POST /api/mindmaps
+export const writeMindmap = async (ctx) => {
+  //
+};
+
+/* ------------------------- 개별 마인드맵 조회 (= 전체 노드 조회) ------------------------ */
+// GET /api/mindmaps/:mindmapId
+export const readMindmap = async (ctx) => {
+  //
+};
+
+/* --------------------------- 개별 마인드맵 수정 (타이틀 수정) -------------------------- */
+// PATCH /api/mindmaps/:mindmapId
+export const udpateMindmap = async (ctx) => {
+  //
+};
+
+/* ------------------------------- 개별 마인드맵 삭제 ------------------------------- */
+// DELETE /api/mindmaps/:mindmapId
+export const removeMindmap = async (ctx) => {
+  //
+};
+
+/* -------------------------------- 개별 노드 작성 -------------------------------- */
+// POST /api/mindmaps/:mindmapId
+export const writeNode = async (ctx) => {
+  //
+};
+
+/* -------------------------------- 개별 노드 조회 -------------------------------- */
+// GET /api/mindmaps/:mindmapId/:nodeId
+export const readNode = async (ctx) => {
+  //
+};
+
+/* -------------------------------- 개별 노드 수정 -------------------------------- */
+// PATCH /api/mindmaps/:mindmapId/:nodeId
+export const updateNode = async (ctx) => {
+  //
+};
+
+/* -------------------------------- 개별 노드 삭제 -------------------------------- */
+// DELETE /api/mindmaps/:mindmapId/:nodeId
+export const removeNode = async (ctx) => {
+  //
+};
