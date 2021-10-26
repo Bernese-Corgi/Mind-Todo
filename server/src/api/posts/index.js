edb81dafb8d6a05b561fb34480146ec824aed7f9
Bernese-Corgi <jnyng0621@gmail.com>
2021-10-27 04:34:59 +0900

    fix: Correct import name

--- server/src/api/posts/index.js
+++ server/src/api/posts/index.js
@@ -1,29 +1,19 @@
 import Router from 'koa-router';
 import * as postsCtrl from './posts.ctrl';
 import { getById } from '../../utils';
-import * as checkUser from '../../utils/checkUser';
+import { checkLoggedIn, checkOwnPost } from '../../utils/checkUser';
 import { Post } from '../../model';
 
 const posts = new Router();
 
 posts.get('/', postsCtrl.list);
-posts.post('/', checkUser.checkLoggedIn, postsCtrl.write);
+posts.post('/', checkLoggedIn, postsCtrl.write);
 
 const post = new Router();
 
 post.get('/', postsCtrl.read);
-post.patch(
-  '/',
-  checkUser.checkLoggedIn,
-  checkUser.checkOwnPost(Post),
-  postsCtrl.update,
-);
-post.delete(
-  '/',
-  checkUser.checkLoggedIn,
-  checkUser.checkOwnPost(Post),
-  postsCtrl.remove,
-);
+post.patch('/', checkLoggedIn, checkOwnPost(Post), postsCtrl.update);
+post.delete('/', checkLoggedIn, checkOwnPost(Post), postsCtrl.remove);
 
 posts.use('/:id', getById(Post), post.routes());
 
