9f2d40fde1aeb5e3c38df02318e98495f76b9e77
Bernese-Corgi <jnyng0621@gmail.com>
2021-10-27 19:11:03 +0900

    feat: Modify mindmap schema

--- server/src/model/mindmap.js
+++ server/src/model/mindmap.js
@@ -1,7 +1,14 @@
 import { model, Schema } from 'mongoose';
 
-const TreeSchema = new Schema({
-  name: String,
+const NodeSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  mindmapId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Mindmap',
+  },
   post: {
     type: Schema.Types.ObjectId,
     ref: 'Post',
@@ -12,7 +19,18 @@ const TreeSchema = new Schema({
       ref: 'Todo',
     },
   ],
-  children: [TreeSchema],
+});
+
+const TreeSchema = new Schema({
+  nodeId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Node',
+  },
+  parentId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Node',
+    default: null,
+  },
 });
 
 const MindmapSchema = new Schema({
@@ -25,7 +43,8 @@ const MindmapSchema = new Schema({
     required: true,
   },
   body: {
-    type: TreeSchema,
+    type: [TreeSchema],
+    // TODO default값 추가
   },
   createdAt: {
     type: Date,
@@ -33,6 +52,7 @@ const MindmapSchema = new Schema({
   },
 });
 
+export const Node = model('Node', NodeSchema);
 export const Tree = model('Tree', TreeSchema);
 
 const Mindmap = model('Mindmap', MindmapSchema);
