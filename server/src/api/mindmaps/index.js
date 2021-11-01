810cc9a200a7a8292e6468e9ec903d13a51d9e5d
Bernese-Corgi <jnyng0621@gmail.com>
2021-11-01 20:36:30 +0900

    feat: Add check user utils in mindmap routes

--- server/src/api/mindmaps/index.js
+++ server/src/api/mindmaps/index.js
@@ -2,37 +2,43 @@ import Router from 'koa-router';
 import { Mindmap } from '../../model';
 import { Node } from '../../model/Mindmap';
 import { getById } from '../../utils';
+import { checkLoggedIn, checkOwn } from '../../utils/checkUser';
 import * as mindmapsCtrl from './mindmaps.ctrl';
 
 /* --------------------------------- 마인드맵 목록 -------------------------------- */
 const mindmaps = new Router();
 // 마인드맵 목록 조회
-mindmaps.get('/', mindmapsCtrl.listMindmap);
+mindmaps.get('/', checkLoggedIn, mindmapsCtrl.listMindmap);
 // 마인드맵 작성
-mindmaps.post('/', mindmapsCtrl.writeMindmap);
+mindmaps.post('/', checkLoggedIn, mindmapsCtrl.writeMindmap);
 
 /* --------------------------------- 개별 마인드맵 -------------------------------- */
 const mindmap = new Router();
 // 개별 마인드맵 조회 (= 전체 노드 조회)
 mindmap.get('/', mindmapsCtrl.readMindmap);
 // 개별 마인드맵 수정 (타이틀 수정)
-mindmap.patch('/', mindmapsCtrl.udpateMindmap);
+mindmap.patch('/', checkOwn(Mindmap), mindmapsCtrl.udpateMindmap);
 // 개별 마인드맵 삭제
-mindmap.delete('/', mindmapsCtrl.removeMindmap);
+mindmap.delete('/', checkOwn(Mindmap), mindmapsCtrl.removeMindmap);
 // 개별 노드 작성
-mindmap.post('/', mindmapsCtrl.writeNode);
+mindmap.post('/', checkOwn(Mindmap), mindmapsCtrl.writeNode);
 // /api/mindmaps/:mindmapId
-mindmaps.use('/:mindmapId', getById(Mindmap), mindmap.routes());
+mindmaps.use('/:mindmapId', getById(Mindmap), checkLoggedIn, mindmap.routes());
 
 /* ---------------------------------- 개별 노드 --------------------------------- */
 const node = new Router();
 // 개별 노드 조회
 node.get('/', mindmapsCtrl.readNode);
 // 개별 노드 수정
-node.patch('/', mindmapsCtrl.updateNode);
+node.patch('/', checkOwn(Node), mindmapsCtrl.updateNode);
 // 개별 노드 삭제
-node.delete('/', mindmapsCtrl.removeNode);
+node.delete('/', checkOwn(Node), mindmapsCtrl.removeNode);
 // /api/mindmaps/:mindmapId/:nodeId
-mindmaps.use('/:mindmapId/:nodeId', getById(Node), node.routes());
+mindmaps.use(
+  '/:mindmapId/:nodeId',
+  getById(Node),
+  checkLoggedIn,
+  node.routes(),
+);
 
 export default mindmaps;
