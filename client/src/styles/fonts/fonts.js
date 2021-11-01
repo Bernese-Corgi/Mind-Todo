1f4dcdb0a8c2ec60ee1e8552b8e65d89d90fc253
Bernese-Corgi <jnyng0621@gmail.com>
2021-11-01 22:44:22 +0900

    feat: Add Noto Sans Kr font

--- client/src/styles/fonts/fonts.js
+++ client/src/styles/fonts/fonts.js
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 import RalewayFont from 'styles/fonts/raleway.woff';
+import NotoSansKr from 'styles/fonts/noto-sans-kr.woff';
 
 export default createGlobalStyle`
   @font-face {
@@ -7,5 +8,11 @@ export default createGlobalStyle`
     font-style: normal;
     font-weight: 400;
     src: url(${RalewayFont}) (format('woff'));
+  }
+  @font-face {
+    font-family: 'Noto Sans KR';
+    font-style: normal;
+    font-weight: 400;
+    src: url(${NotoSansKr}) (format('woff'));
   }
 `;
