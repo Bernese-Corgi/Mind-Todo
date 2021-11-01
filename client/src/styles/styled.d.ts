671057781777186f3c20f575db003275af56371e
Bernese-Corgi <jnyng0621@gmail.com>
2021-11-01 22:53:33 +0900

    feat: Add global styles

---
+++ client/src/styles/styled.d.ts
@@ -0,0 +1,83 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      primary: {
+        base: string;
+        light: string;
+        dark: string;
+      };
+      secondary: {
+        base: string;
+        light: string;
+        dark: string;
+      };
+      gray: {
+        base: string;
+        light: string;
+        dark: string;
+      };
+      white: string;
+      red: string;
+      orange: string;
+      yellow: string;
+      green: string;
+      blue: string;
+      purple: string;
+      pink: string;
+      rainbow: array;
+    };
+    fonts: {
+      family: {
+        base: string;
+        title: string;
+      };
+      size: {
+        sm: string;
+        base: string;
+        lg: string;
+        xl: string;
+        title: string;
+      };
+      weight: {
+        light: number;
+        normal: number;
+        bold: number;
+      };
+    };
+    margins: {
+      sm: string;
+      base: string;
+      lg: string;
+      xl: string;
+    };
+    paddings: {
+      sm: string;
+      base: string;
+      lg: string;
+      xl: string;
+    };
+    boxShadow: {
+      default: string;
+    };
+    borders: {
+      radius: {
+        round: string;
+        square: string;
+      };
+    };
+    flexes: {
+      mixin: (direction: string, align: string, justify: string) => string;
+      center: string;
+      row: (justify: string) => string;
+      column: (justify: string) => string;
+    };
+    positions: {
+      absolute: {
+        topLeft: string;
+        center: string;
+      };
+    };
+  }
+}
---
+++ client/src/styles/theme.ts
@@ -0,0 +1,124 @@
+import { DefaultTheme } from 'styled-components';
+
+const colors = {
+  primary: {
+    base: '#D2B48C',
+    light: '#DFD9D3',
+    dark: '#645645',
+  },
+  secondary: {
+    base: '#7B86AA',
+    light: '#BFC8EA',
+    dark: '#17243B',
+  },
+  gray: {
+    base: '#a5a3a4',
+    light: '#DCDCDC',
+    dark: '#545051',
+  },
+  white: '#F8F8FF',
+  red: '#DC143C',
+  orange: '#FF7F00',
+  yellow: '#F89B00',
+  green: '#008D62',
+  blue: '#00498C',
+  purple: '#660099',
+  pink: '#F29886',
+  rainbow: ['#eaba9b', '#ffd789', '#b3beaa', '#8caad0', '#aeaebf'],
+};
+
+const fonts = {
+  family: {
+    base: `'Noto Sans KR', sans-serif`,
+    title: 'Raleway',
+  },
+  size: {
+    sm: '1.4rem',
+    base: '1.6rem',
+    lg: '2rem',
+    xl: '2.5rem',
+    title: '6rem',
+  },
+  weight: {
+    light: 200,
+    normal: 400,
+    bold: 700,
+  },
+};
+
+const margins = {
+  sm: '0.5rem',
+  base: '1rem',
+  lg: '2rem',
+  xl: '3rem',
+};
+
+const paddings = {
+  sm: '0.8rem',
+  base: '1rem',
+  lg: '1.5rem',
+  xl: '3rem',
+};
+
+const boxShadow = {
+  default: '0 0.4em 1.5em rgba(0, 0, 0, 0.2)',
+};
+
+const borders = {
+  radius: {
+    round: '50px',
+    square: '4px',
+  },
+};
+
+const flexes = {
+  mixin: (direction = 'row', align = 'center', justify = 'center') => `
+    display:flex;
+    flex-direction:${direction};
+    align-items:${align};
+    justify-content:${justify}
+  `,
+  center: `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `,
+  row: (justify = 'center') => `
+    display: flex;
+    justify-content: ${justify};
+    align-items: center;
+  `,
+  column: (justify = 'center') => `
+    display: flex;
+    flex-flow: column;
+    justify-content: ${justify};
+    align-items: center;
+  `,
+};
+
+const positions = {
+  absolute: {
+    topLeft: `
+      position: absolute;
+      top: 0;
+      left: 0;
+    `,
+    center: `
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translateX(-50%) translateY(-50%);
+    `,
+  },
+};
+
+export const theme: DefaultTheme = {
+  colors,
+  fonts,
+  margins,
+  paddings,
+  boxShadow,
+  borders,
+  flexes,
+  positions,
+};
