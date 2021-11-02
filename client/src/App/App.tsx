b58a6c4c0f062f8742bc0d1f9d817498ecb6e11b
Bernese-Corgi <jnyng0621@gmail.com>
2021-11-02 23:57:48 +0900

    feat: Add Home page and Add Router to home page in App component

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,7 +1,14 @@
+import { HomePage } from 'pages';
+import { Route } from 'react-router';
+
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Route component={HomePage} path="/" />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+import { ButtonContainer } from 'container/common';
+import React from 'react';
+import styled from 'styled-components';
+
+const HomePage = () => {
+  return <></>;
+};
+
+export default HomePage;
