f1bc2009e4a641008e38fbf61600a1e6d3b4a581
Bernese-Corgi <jnyng0621@gmail.com>
2021-11-05 19:27:30 +0900

    feat: Add StyledForm component

--- client/src/components/common/index.tsx
+++ client/src/components/common/index.tsx
@@ -4,3 +4,6 @@ export { default as Button } from './Button/Button';
 export { default as Portal } from './Portal/Portal';
 export { default as Dialog } from './Dialog/Dialog';
 export { default as Input } from './Input';
+
+/* styled component ---------------------------- */
+export { default as StyledForm } from './Form.styled';
