7600c0287aea94ed2331633e4115f7ceb004b631
Bernese-Corgi <jnyng0621@gmail.com>
2021-11-12 17:41:07 +0900

    feat: Apply loading reducer to root reducer

--- client/src/redux/modules/index.tsx
+++ client/src/redux/modules/index.tsx
@@ -3,6 +3,7 @@ import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import authReducer from './auth/auth';
 import userReducer from './auth/user';
+import loadingReducer from './loading';
 
 // 리덕스 상태 지속 설정
 const persistConfig = {
@@ -14,6 +15,7 @@ const persistConfig = {
 const rootReducer = combineReducers({
   auth: authReducer,
   user: userReducer,
+  loading: loadingReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
