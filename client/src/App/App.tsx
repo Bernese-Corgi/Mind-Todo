9ea0dd7de9e181fb8a7a127fb3f638b62292eb10
Bernese-Corgi <jnyng0621@gmail.com>
2021-11-12 17:51:53 +0900

    feat: Add Route component for sign-in, sign-up dialog in App component

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,12 +1,46 @@
+import { Dialog } from 'components/common';
+import SignInForm from 'container/auth/SignInForm';
+import SignUpForm from 'container/auth/SignUpForm';
 import { HomePage } from 'pages';
 import { Route } from 'react-router';
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Route component={HomePage} path="/" />
+
+      <Route
+        render={({ history }) => {
+          return (
+            <Dialog
+              hasModal
+              visible
+              onClose={() => {
+                history.goBack();
+              }}>
+              <SignUpForm />
+            </Dialog>
+          );
+        }}
+        path="/sign-up"
+      />
+
+      <Route
+        render={({ history }) => {
+          return (
+            <Dialog
+              hasModal
+              visible
+              onClose={() => {
+                history.goBack();
+              }}>
+              <SignInForm />
+            </Dialog>
+          );
+        }}
+        path="/sign-in"
+      />
     </div>
   );
 }
