46e8f82cf3f0e840aab14ddb4fbeefc3047fcd88
Bernese-Corgi <jnyng0621@gmail.com>
2021-11-15 23:29:04 +0900

    feat: Add useReduxDispatch hook in redux store

--- client/src/redux/store.tsx
+++ client/src/redux/store.tsx
@@ -1,10 +1,10 @@
-import { applyMiddleware, createStore } from 'redux';
+import { Action, applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
-import reduxThunk from 'redux-thunk';
+import reduxThunk, { ThunkDispatch } from 'redux-thunk';
 import { persistStore } from 'redux-persist';
-import persistedReducer from './modules';
-import { Provider } from 'react-redux';
+import persistedReducer, { RootState } from './modules';
+import { Provider, useDispatch } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 const middlewares = [reduxThunk, logger];
@@ -13,6 +13,10 @@ const store = createStore(
   persistedReducer,
   composeWithDevTools(applyMiddleware(...middlewares))
 );
+
+export type ReduxDispatch = ThunkDispatch<RootState, any, Action>;
+
+export const useReduxDispatch = () => useDispatch<ReduxDispatch>();
 
 const persistor = persistStore(store);
 
