0b373b01ef3c3198a515fc552977ff89e214d3de
Bernese-Corgi <jnyng0621@gmail.com>
2021-11-15 23:27:33 +0900

    feat: Add writeNode axios api

--- client/src/utils/api/mindmaps.ts
+++ client/src/utils/api/mindmaps.ts
@@ -6,9 +6,17 @@ export type Mindmap = {
   title: string;
 };
 
+export type NodeType = {
+  name: string;
+  parentId?: string; // | null
+};
+
 export const listMindmap = () => client.get(`${URI}`);
 
 export const writeMindmap = (mindmap: Mindmap) =>
   client.post(`${URI}`, mindmap);
 
 export const readMindmap = (id: string) => client.get(`${URI}/${id}`);
+
+export const writeNode = (mindmapId: string, newNode: NodeType) =>
+  client.post(`${URI}/${mindmapId}`, newNode);
