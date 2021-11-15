6798fc219d77085717bf49412bc0a30d1b7468b2
Bernese-Corgi <jnyng0621@gmail.com>
2021-11-15 23:26:44 +0900

    fix: Fix bugs to initializeForm in AddMindmapDialogContainer

--- client/src/container/mindmaps/AddMindmapDialogContainer.tsx
+++ client/src/container/mindmaps/AddMindmapDialogContainer.tsx
@@ -1,32 +1,33 @@
-import AddMindmapDialog from 'components/mindmaps/AddMindmapDialog';
 import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { LoadingIcon } from 'components/common';
+import { AddMindmapDialog } from 'components/mindmaps';
+import { useSelector } from 'react-redux';
 import { withRouter } from 'react-router';
 import { RootState } from 'redux/modules';
-import { finishLoading } from 'redux/modules/loading';
 import {
   initializeMindmapForm,
   writeMindmapAsync,
 } from 'redux/modules/mindmaps/mindmap';
-import { Mindmap } from 'utils/api/mindmaps';
+import { useReduxDispatch } from 'redux/store';
 
 const AddMindmapDialogContainer = ({ history }) => {
-  const dispatch = useDispatch();
-  const { mindmap, loading } = useSelector((state: RootState) => state);
+  const dispatch = useReduxDispatch();
+  const { mindmap } = useSelector((state: RootState) => state);
 
-  const [values, setValues] = useState<Mindmap>({
+  const [values, setValues] = useState({
     title: '',
   });
 
   const [error, setError] = useState('');
 
-  // useEffect(() => {
-  //   console.log('useEffect 첫번째');
-  //   dispatch(initializeMindmapForm());
-  // }, []);
+  useEffect(() => {
+    dispatch(initializeMindmapForm());
+  }, [dispatch]);
 
   const handleChangeTitle = (e: ChangeEvent<HTMLInputElement>) => {
-    setValues({ ...values, title: e.target.value });
+    const { value } = e.target;
+
+    setValues({ ...values, title: value });
 
     if (values.title) {
       setError('');
@@ -48,45 +49,29 @@ const AddMindmapDialogContainer = ({ history }) => {
     },
   };
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    dispatch(initializeMindmapForm());
-
-    console.log('handle Submit');
 
     if (!values.title) {
       setError('마인드맵 제목을 입력해주세요');
       return;
     }
 
-    dispatch(writeMindmapAsync(values));
+    const newMindmap = await dispatch(writeMindmapAsync(values));
 
-    console.log(mindmap.data);
-
-    // if (mindmap.data) {
-    //   console.log(
-    //     'mindmap data가 있니??? 그럼 history push가 실행된단다',
-    //     mindmap.data
-    //   );
-    //   history.push(`/mindmap/${mindmap.data?._id}`);
-    // }
-
-    console.log(finishLoading('mindmap/WRITE_MINDMAP'));
+    if (newMindmap) {
+      history.push(`/mindmap/${newMindmap._id}`);
+    }
   };
 
   useEffect(() => {
-    //
-  }, []);
-
-  useEffect(() => {
-    console.log('useEffect 두번째');
     if (mindmap.error) {
       setError('마인드맵 생성에 실패했습니다. 다시 시도해주세요');
       return;
     }
-
-    // write mindmap이 잘 됐으면 history.push('/:mindmapsId')
   }, [dispatch, history, mindmap.data, mindmap.error]);
+
+  if (mindmap.loading) return <LoadingIcon />;
 
   return (
     <>
