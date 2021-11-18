f149b2a602be62f437042ec70f2e8935535c1f1a
Bernese-Corgi <jnyng0621@gmail.com>
2021-11-18 20:48:24 +0900

    feat: Add a11y-hidden style in global style

--- client/src/styles/global-style.ts
+++ client/src/styles/global-style.ts
@@ -167,6 +167,17 @@ export const GlobalStyle = createGlobalStyle`
     content: '';
     content: none;
   }
+
+  .a11yHidden {
+    overflow: hidden;
+    position: absolute;
+    clip: rect(0 0 0 0);
+    clip-path: polygon(0 0, 0 0, 0 0);
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    white-space: nowrap;
+  }
 `;
 
 export default GlobalStyle;
