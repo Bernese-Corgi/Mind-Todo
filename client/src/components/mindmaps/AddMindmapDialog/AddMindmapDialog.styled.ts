4da4d13ed76caccb69edbd3ddc60d9b44eb4399c
Bernese-Corgi <jnyng0621@gmail.com>
2021-11-19 23:56:38 +0900

    feat: Modify AddMindmapDialog's directory and Add layout styles

---
+++ client/src/components/mindmaps/AddMindmapDialog/AddMindmapDialog.styled.ts
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+import theme from 'styles/theme';
+import { AddMindmapDialogProps } from './AddMindmapDialog';
+
+export const addMindmapDialogStyle = `
+  background: ${theme.colors.white};
+  border-radius: ${theme.borders.radius.square};
+  width: 50vw;
+  min-height: 180px;
+  font-size: ${theme.fonts.size.sm};
+`;
+export const AddMindmapDialogWrapper = styled.div`
+  ${theme.flexes.mixin('column', 'start', 'start')}
+  width: 100%;
+  height: 100%;
+  padding: 1.8em;
+
+  h2 {
+    font-size: 0.85em;
+    margin-left: 0.5em;
+    margin-bottom: 1em;
+  }
+`;
+
+export const StyledMindmapForm = styled.form<AddMindmapDialogProps>`
+  width: 95%;
+  margin: 0 auto 0 auto;
+
+  .inputLabelWrapper {
+    width: 100%;
+  }
+
+  input {
+    ${theme.defElem.input()}
+    margin-bottom: 0.2em;
+  }
+
+  .errorInput {
+    ${theme.defElem.errInput}
+  }
+
+  .addMindmapDialogBtnWrapper {
+    margin-top: 0.8em;
+    ${theme.flexes.row('flex-end')}
+
+    button {
+      font-size: 80%;
+      margin-right: 0.8em;
+    }
+  }
+`;
+
+AddMindmapDialogWrapper.displayName = 'AddMindmapDialogWrapper';
+StyledMindmapForm.displayName = 'StyledMindmapForm';
