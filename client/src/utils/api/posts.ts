46140a1738180551689fca1d8ef959ba08efb242
Bernese-Corgi <jnyng0621@gmail.com>
2021-11-22 23:42:31 +0900

    feat: Add writePost, readPost axios api

--- client/src/utils/api/posts.ts
+++ client/src/utils/api/posts.ts
@@ -2,6 +2,13 @@ import client from './client';
 
 const URI = '/api/posts';
 
-export const writePost = (nodeId: string) => client.post(`${URI}/${nodeId}`);
+export type Post = {
+  title: string;
+  body: string;
+  tags?: string[];
+};
 
+export const writePost = (nodeId: string, newPost: Post) =>
+  client.post(`${URI}/${nodeId}`, newPost);
 
+export const readPost = (postId: string) => client.get(`${URI}/${postId}`);
