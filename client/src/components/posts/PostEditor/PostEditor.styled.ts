cbd7371275668a6c3a33f5073ebf9bc6888d81d4
Bernese-Corgi <jnyng0621@gmail.com>
2021-11-23 16:08:31 +0900

    style: Add layout style in PostEditor component

--- client/src/components/posts/PostEditor/PostEditor.styled.ts
+++ client/src/components/posts/PostEditor/PostEditor.styled.ts
@@ -1,5 +1,44 @@
 import styled from 'styled-components';
+import theme from 'styles/theme';
 
-export const PostEditorWrapper = styled.div`
-  /*  */
+export const StyledPostEditorForm = styled.form`
+  width: 100%;
+  height: 100%;
+  font-size: ${theme.fonts.size.base};
+
+  .inputLabelWrapper,
+  .mdEditorWrapper {
+    margin-top: 1.5em;
+
+    label {
+      font-size: 0.9em;
+      margin-left: 0.3em;
+      margin-bottom: 0.5em;
+    }
+
+    ${theme.flexes.mixin('column', 'start', 'start')}
+  }
+
+  #postTitleInput,
+  #postBodyInput {
+    ${theme.defElem.input()}
+  }
+
+  #postBodyInput {
+  }
+
+  .btnWrapper {
+    font-size: 0.9em;
+    width: max-content;
+    margin: 1.5em auto 1.5em auto;
+
+    button {
+      margin-left: 1em;
+      margin-right: 1em;
+
+      &:hover {
+        box-shadow: ${theme.boxShadow.narrow};
+      }
+    }
+  }
 `;
--- client/src/components/posts/PostEditor/PostEditor.tsx
+++ client/src/components/posts/PostEditor/PostEditor.tsx
@@ -1,45 +1,44 @@
 import { Input, Button, MdEditor } from 'components/common';
 import React from 'react';
-import { PostEditorWrapper } from './PostEditor.styled';
+import { StyledPostEditorForm } from './PostEditor.styled';
 
-const PostEditor = ({ onSubmit, values, errors, onChanges, onClicks }) => {
-  return (
-    <PostEditorWrapper>
-      <form onSubmit={onSubmit}>
-        <Input
-          id="postTitleInput"
-          label="제목"
-          type="text"
-          name="title"
-          value={values.title}
-          errorMsg={errors.body}
-          placeholder="제목을 입력하세요."
-          autoComplete="off"
-          onChange={onChanges.title}
-        />
-        <MdEditor
-          id="postBodyInput"
-          label="내용"
-          name="body"
-          value={values.body}
-          onChange={onChanges.body}
-        />
-        <Button
-          id="cancelWritePost"
-          title="글 작성 취소"
-          onClick={onClicks.cancelBtn}
-          children="취소"
-        />
-        <Button
-          type="submit"
-          id="submitPost"
-          title="작성글 등록하기"
-          primary
-          children="등록"
-        />
-      </form>
-    </PostEditorWrapper>
-  );
-};
+const PostEditor = ({ onSubmit, values, errors, onChanges, onClicks }) => (
+  <StyledPostEditorForm onSubmit={onSubmit}>
+    <Input
+      id="postTitleInput"
+      label="제목"
+      type="text"
+      name="title"
+      value={values.title}
+      errorMsg={errors.body}
+      placeholder="제목을 입력해주세요."
+      autoComplete="off"
+      onChange={onChanges.title}
+    />
+    <MdEditor
+      id="postBodyInput"
+      label="내용"
+      name="body"
+      value={values.body}
+      placeholder="내용을 입력해주세요."
+      onChange={onChanges.body}
+    />
+    <div className="btnWrapper">
+      <Button
+        id="cancelWritePost"
+        title="글 작성 취소"
+        onClick={onClicks.cancelBtn}
+        children="취소"
+      />
+      <Button
+        type="submit"
+        id="submitPost"
+        title="작성글 등록하기"
+        primary
+        children="등록"
+      />
+    </div>
+  </StyledPostEditorForm>
+);
 
 export default PostEditor;
