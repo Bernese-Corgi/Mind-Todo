73394e0afe67712728c14728438a12041740f14d
Bernese-Corgi <jnyng0621@gmail.com>
2021-11-25 20:44:07 +0900

    feat: Add util funtion that adds <tspan> element to child of <text> element (To solve the problem that can't wrap text when using <text> element of svg

--- client/src/utils/mindmap.ts
+++ client/src/utils/mindmap.ts
@@ -1,4 +1,5 @@
 import { stratify } from 'd3-hierarchy';
+import { chunkString } from './stringUtils';
 
 export const stratifiedMindmap = treeData => {
   const stratifiedData = stratify()
@@ -6,4 +7,32 @@ export const stratifiedMindmap = treeData => {
     .parentId((d: any) => d.parent.name)(treeData);
 
   return stratifiedData;
+};
+
+export const wrapText = (
+  selection,
+  text: string,
+  splitLength: number = 10,
+  coordinates?: { x?: number | string; y?: number }
+) => {
+  // 매개변수 1. 자를 텍스트 길이
+  // 매개변수 2. selection
+  // 매개변수 3. 자를 텍스트
+  const _x = coordinates?.x ? coordinates?.x : 0;
+  const _y = coordinates?.y ? coordinates?.y : 14;
+
+  const slicedTexts = chunkString(text, splitLength);
+
+  const firstText = text.slice(0, splitLength);
+
+  selection.text(firstText).attr('x', _x);
+
+  slicedTexts?.map((t, i) => {
+    if (i === 0) return;
+    selection
+      .append('tspan')
+      .text(t)
+      .attr('x', _x)
+      .attr('y', _y * (i + 1));
+  });
 };
