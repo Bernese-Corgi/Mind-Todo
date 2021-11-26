bb1894b6331482e0b5ed26095e184327dc1fd8a6
Bernese-Corgi <jnyng0621@gmail.com>
2021-11-26 19:12:20 +0900

    feat: Add MindmapNode component

--- client/src/components/mindmaps/index.tsx
+++ client/src/components/mindmaps/index.tsx
@@ -2,3 +2,4 @@ export { default as AddMindmapDialog } from './AddMindmapDialog/AddMindmapDialog
 export { default as Mindmap } from './Mindmap/Mindmap';
 export { default as MindmapList } from './MindmapList';
 export { default as AddNodeDialog } from './AddNodeDialog/AddNodeDialog';
+export { default as MindmapNode } from './MindmapNode/MindmapNode';
