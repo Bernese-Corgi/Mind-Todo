05c353f4cffec9d2bf9a2fd80b24200976dfeeb4
Bernese-Corgi <jnyng0621@gmail.com>
2021-11-27 17:17:41 +0900

    feat: Add some props for tags in AddPost component

--- client/src/components/posts/AddPost/AddPost.tsx
+++ client/src/components/posts/AddPost/AddPost.tsx
@@ -3,23 +3,31 @@ import React from 'react';
 import { PostEditor, PostViewer } from '..';
 import { StyledAddPostArticle } from './AddPost.styled';
 
-const AddPost = ({ values, errors, onSubmit, onChanges, onClicks }) => {
+const AddPost = ({
+  values,
+  errors,
+  localTags,
+  onSubmit,
+  onChanges,
+  onClicks,
+}) => {
   return (
     <Responsive>
       <StyledAddPostArticle>
         <section className="section postEditor">
-          <h3>글 작성</h3>
+          <h3 className="postEditorHeader">글 작성</h3>
           <PostEditor
             values={values}
             errors={errors}
+            localTags={localTags}
             onSubmit={onSubmit}
             onChanges={onChanges}
             onClicks={onClicks}
           />
         </section>
         <section className="section postViewer">
-          <h3>미리보기</h3>
-          <PostViewer post={values} />
+          <h3 className="postEditorHeader">미리보기</h3>
+          <PostViewer post={values} localTags={localTags} />
         </section>
       </StyledAddPostArticle>
     </Responsive>
