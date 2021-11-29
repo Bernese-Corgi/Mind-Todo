9be80c446bb3e1b468a8278e6d3bb690bb3198d8
Bernese-Corgi <jnyng0621@gmail.com>
2021-11-30 02:36:35 +0900

    feat: Add default style component style in MdViewer component

--- client/src/components/common/MdViewer/MdViewer.styled.ts
+++ client/src/components/common/MdViewer/MdViewer.styled.ts
@@ -1,38 +1,131 @@
 import styled, { css } from 'styled-components';
 import theme from 'styles/theme';
 
-export const MdViewerWrapper = styled.article``;
-
-const defaultHGroupStyle = css`
-  color: ${theme.colors.primary.dark};
+const defaultText = css`
+  font-size: 1em;
   margin: 0.5em 0 0.5em 0;
+  padding: 0;
 `;
 
-export const MdH1 = styled.h1`
-  ${defaultHGroupStyle}
-  font-size: 1.5em;
-`;
+export const MdViewerWrapper = styled.article`
+  font-size: 1em !important;
 
-export const MdH2 = styled.h2`
-  ${defaultHGroupStyle}
-  font-size: 1.4em;
-`;
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    color: ${theme.colors.primary.dark};
+    margin: 1.5em 0 0.8em 0;
+  }
 
-export const MdH3 = styled.h3`
-  ${defaultHGroupStyle}
-  font-size: 1.3em;
-`;
+  h1 {
+    font-size: 1.5em;
+  }
+  h2 {
+    font-size: 1.4em;
+  }
+  h3 {
+    font-size: 1.3em;
+  }
+  h4 {
+    font-size: 1.2em;
+  }
+  h5 {
+    font-size: 1.1em;
+  }
 
-export const MdH4 = styled.h4`
-  ${defaultHGroupStyle}
-  font-size: 1.2em;
-`;
+  li {
+    ${defaultText}
+    padding-left: 0.5em;
+  }
 
-export const MdH5 = styled.h5`
-  ${defaultHGroupStyle}
-  font-size: 1.1em;
-`;
+  ol {
+    li {
+      list-style: decimal inside;
+    }
+  }
 
-export const MdH6 = styled.h6`
-  ${defaultHGroupStyle}
+  ul {
+    font-size: 1em;
+    padding: 0;
+
+    li {
+      list-style: inside;
+    }
+  }
+
+  p {
+    ${defaultText}
+  }
+
+  a {
+    color: ${theme.colors.blue}98;
+
+    &:hover {
+      color: ${theme.colors.blue};
+      text-decoration: underline;
+    }
+
+    &:link {
+      color: ${theme.colors.blue}98;
+    }
+
+    &:visited {
+      color: ${theme.colors.gray.dark}98;
+    }
+
+    &:active {
+      color: ${theme.colors.purple};
+      text-decoration: underline;
+    }
+  }
+
+  em {
+    font-style: italic;
+    color: ${theme.colors.primary.dark};
+  }
+
+  strong {
+    font-weight: ${theme.fonts.weight.bold};
+    color: ${theme.colors.primary.dark};
+  }
+
+  del {
+    color: ${theme.colors.gray.base};
+  }
+
+  blockquote {
+    background-color: ${theme.colors.gray.light}80;
+    padding: 0.5em 1em;
+    margin: 1em 0.3em;
+  }
+
+  mark {
+    background-color: ${theme.colors.gray.base};
+  }
+
+  table {
+    width: 100%;
+    border-top: 1px solid ${theme.colors.gray.dark};
+    border-collapse: collapse;
+    margin: 1em 0.3em;
+  }
+
+  th,
+  td {
+    border-bottom: 1px solid ${theme.colors.gray.base};
+    border-left: 1px solid ${theme.colors.gray.base};
+    padding: 10px;
+  }
+
+  th {
+    background-color: ${theme.colors.gray.light}70;
+  }
+
+  th:first-child,
+  td:first-child {
+    border-left: none;
+  }
 `;
