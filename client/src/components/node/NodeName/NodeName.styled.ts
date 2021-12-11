3fcc0ab9b50616edaeb05d8d6fbcaabef964af4c
Bernese-Corgi <jnyng0621@gmail.com>
2021-12-11 12:28:37 +0900

    feat: Add NodeName component

---
+++ client/src/components/node/NodeName/NodeName.styled.ts
@@ -0,0 +1,9 @@
+import { EditDeleteButtonUnit } from 'components/common';
+import styled from 'styled-components';
+import theme from 'styles/theme';
+
+export const StyledNodeNameEditUnit = styled(EditDeleteButtonUnit)`
+  .editTextArea {
+    font-weight: ${theme.fonts.weight.bold};
+  }
+`;
---
+++ client/src/components/node/index.tsx
@@ -0,0 +1,1 @@
+export { default as NodeName } from './NodeName/NodeName';
