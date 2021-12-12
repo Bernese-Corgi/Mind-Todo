d3ba6de9c0177eda7e6bb69e4f8be3a97f2c718a
Bernese-Corgi <jnyng0621@gmail.com>
2021-12-12 16:54:29 +0900

    feat: Add nodeList api of todos controller and Modify id name of todos api in server

--- server/src/api/todos/index.js
+++ server/src/api/todos/index.js
@@ -8,11 +8,12 @@ const todos = new Router();
 
 todos.get('/', checkLoggedIn, todosCtrl.list);
 todos.post('/:nodeId', checkLoggedIn, todosCtrl.write);
+todos.get('/:nodeId', checkLoggedIn, todosCtrl.nodeList);
 
 const todo = new Router();
 todo.patch('/', checkLoggedIn, checkOwn(Todo), todosCtrl.update);
 todo.delete('/', checkLoggedIn, checkOwn(Todo), todosCtrl.remove);
 
-todos.use('/:id', getById(Todo), todo.routes());
+todos.use('/:todoId', getById(Todo), todo.routes());
 
 export default todos;
