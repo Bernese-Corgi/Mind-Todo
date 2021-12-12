bce95320f258151986383e87e198ecabff66f3e2
Bernese-Corgi <jnyng0621@gmail.com>
2021-12-12 17:51:02 +0900

    feat: Add chunkDateString and changeFirstStrToUpper util function in stringUtils

--- client/src/utils/stringUtils.ts
+++ client/src/utils/stringUtils.ts
@@ -1,2 +1,8 @@
 export const chunkString = (str: string, length: number) =>
   str.match(new RegExp('.{1,' + length + '}', 'g'));
+
+export const chunkDateString = (date: string) =>
+  date.slice(0, 10).replaceAll('-', ' / ');
+
+export const changeFirstStrToUpper = (str: string) =>
+  str[0].toUpperCase() + str.slice(1);
