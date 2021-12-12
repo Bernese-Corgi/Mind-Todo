28f37880794305e5d02507130b19be0854767f98
Bernese-Corgi <jnyng0621@gmail.com>
2021-12-12 17:53:04 +0900

    feat: Add export statement in index files

--- client/src/components/posts/index.tsx
+++ client/src/components/posts/index.tsx
@@ -1,3 +1,4 @@
 export { default as AddPost } from './AddPost/AddPost';
 export { default as PostViewer } from './PostViewer/PostViewer';
 export { default as PostEditor } from './PostEditor/PostEditor';
+export { default as PostList } from './PostList';
