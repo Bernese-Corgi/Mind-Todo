1d2fcd1876d3e2c6867c35440e5ab8c733307b79
Bernese-Corgi <jnyng0621@gmail.com>
2021-12-12 17:18:18 +0900

    feat: Apply data function props to event handler in AddTodo component

--- client/src/components/todos/AddTodo/AddTodo.styled.ts
+++ client/src/components/todos/AddTodo/AddTodo.styled.ts
@@ -1,23 +1,53 @@
-import { EditTextArea, InputField } from 'components/common';
 import styled from 'styled-components';
 import theme from 'styles/theme';
+import { handleSvgHoverColor } from 'utils/style';
 
 export const StyledAddTodoForm = styled.form`
   /* TODO fontSize 더 상위로 옮기기 */
   font-size: ${theme.fonts.size.sm};
   width: 100%;
 
-  .editTextAreaWrapper {
-    width: 100%;
+  .todoAddBtn {
+    padding: 0.5em;
+
+    ${handleSvgHoverColor(theme.colors.secondary.base)}
   }
 
-  .todoAddBtn {
+  .openBtn {
+    font-size: 0.8em;
+    background-color: #fff;
+    border: 0.18em solid ${theme.colors.primary.dark}70;
+    color: ${theme.colors.primary.dark}99;
+
+    &:hover {
+      border: 0.18em solid transparent;
+      background-color: ${theme.colors.primary.light}90;
+      color: ${theme.colors.primary.dark};
+    }
+  }
+
+  .closeBtn {
+    padding: 0.5em;
+
+    ${handleSvgHoverColor(theme.colors.primary.dark)}
+
     svg {
-      color: ${theme.colors.secondary.base}90;
+      margin-left: 0.3em;
     }
-    &:hover svg {
-      color: ${theme.colors.secondary.base};
-    }
+
+    ${theme.flexes.row('start')}
+  }
+
+  .editTextAreaWrapper {
+    width: 100%;
+    position: relative;
+  }
+
+  .errorTextArea {
+    position: absolute;
+    top: 2.1em;
+    left: 0.5em;
+    z-index: -1;
   }
 
   ${theme.flexes.row('start')}
--- client/src/components/todos/AddTodo/AddTodo.tsx
+++ client/src/components/todos/AddTodo/AddTodo.tsx
@@ -1,45 +1,94 @@
-import React, { ChangeEvent, FormEvent, useState } from 'react';
+import React, { ChangeEvent, FormEvent, useRef, useState } from 'react';
 import { Button, EditTextArea } from 'components/common';
 import { StyledAddTodoForm } from './AddTodo.styled';
 
 interface AddTodoProps {
-  onSubmit?: (e: FormEvent<HTMLFormElement>) => void;
+  onAddTodo: (content: string) => void;
 }
 
-const AddTodo = ({ onSubmit }: AddTodoProps) => {
-  const initialValues = {
-    content: '',
-  };
+const AddTodo = ({ onAddTodo }: AddTodoProps) => {
+  const addInputRef = useRef<HTMLTextAreaElement>(null);
 
-  const [values, setValues] = useState(initialValues);
+  const [isEdit, setIsEdit] = useState(false);
+
+  const [contentVal, setContentVal] = useState('');
   const [error, setError] = useState('');
 
-  const handleChangeAddTodoInput = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    const { name, value } = e.target;
-    setValues({ ...values, [name]: value });
+  const handleChangeAddTextArea = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setContentVal(e.target.value);
+    setError('');
   };
 
-  const handleSubmt = (e: FormEvent<HTMLFormElement>) => {
+  const handleFocusEdit = () => {
+    setError('');
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // TODO dispatch()
+
+    if (!contentVal) {
+      setError('내용을 입력해주세요.');
+      return;
+    }
+
+    onAddTodo(contentVal);
+
+    setContentVal('');
+    addInputRef.current?.focus();
+  };
+
+  const handleClicks = {
+    openAddTodo: () => {
+      setIsEdit(true);
+      addInputRef.current?.focus();
+    },
+    closeAddTodo: () => {
+      setIsEdit(false);
+    },
   };
 
   return (
-    <StyledAddTodoForm onSubmit={onSubmit}>
-      <EditTextArea
-        id="todoAddInput"
-        name="content"
-        value={values.content}
-        errorMsg={error}
-        onChange={handleChangeAddTodoInput}
-      />
-      <Button
-        type="submit"
-        id="todoAddBtn"
-        title="할 일 추가하기"
-        shape="plus"
-        className="todoAddBtn"
-      />
+    <StyledAddTodoForm onSubmit={handleSubmit}>
+      {isEdit ? (
+        <EditTextArea
+          id="todoAddInput"
+          name="content"
+          value={contentVal}
+          errorMsg={error}
+          className="addTodoTextArea"
+          errorClassName="errorTextArea"
+          onChange={handleChangeAddTextArea}
+          onFocus={handleFocusEdit}
+          ref={addInputRef}
+        />
+      ) : (
+        <Button
+          id="openAddTodo"
+          title="할 일 작성 창 열기"
+          round="round"
+          onClick={handleClicks.openAddTodo}
+          children="할 일 작성"
+          className="openBtn"
+        />
+      )}
+      {isEdit && (
+        <>
+          <Button
+            type="submit"
+            id="todoAddBtn"
+            title="할 일 추가하기"
+            shape="plus"
+            className="todoAddBtn"
+          />
+          <Button
+            id="closeAddTodo"
+            title="창 닫기"
+            onClick={handleClicks.closeAddTodo}
+            shape="cancel"
+            className="closeBtn"
+          />
+        </>
+      )}
     </StyledAddTodoForm>
   );
 };
