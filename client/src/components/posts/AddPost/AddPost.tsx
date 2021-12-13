4c2c2a1ce72672add4d6372854828bda44d9d7a0
Bernese-Corgi <jnyng0621@gmail.com>
2021-12-13 15:03:12 +0900

    refactor: Move event handler and state about DOM to AddPost component, and fix invalid event when remove tag

--- client/src/components/posts/AddPost/AddPost.tsx
+++ client/src/components/posts/AddPost/AddPost.tsx
@@ -1,16 +1,156 @@
+import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import { Responsive } from 'components/common';
-import React from 'react';
 import { PostEditor, PostViewer } from '..';
 import { StyledAddPostArticle } from './AddPost.styled';
+import { RouteComponentProps, withRouter } from 'react-router';
+
+interface AddPostParams {
+  postId?: string;
+  mindmapId?: string;
+  nodeId?: string;
+}
+
+interface AddPostProps {
+  post?: any;
+  postErrMsg?: any;
+  onWrite: (newPost) => void;
+  onEdit: (updatePost) => void;
+}
 
 const AddPost = ({
-  values,
-  errors,
-  localTags,
-  onSubmit,
-  onChanges,
-  onClicks,
-}) => {
+  post,
+  postErrMsg,
+  onWrite,
+  onEdit,
+  history,
+}: AddPostProps & RouteComponentProps<AddPostParams>) => {
+  const initialState = {
+    values: { title: '', body: '', tag: '' },
+    errors: { title: '', body: '', tag: '', post: '' },
+  };
+
+  const [values, setValues] = useState(initialState.values);
+  const [errors, setErrors] = useState(initialState.errors);
+  const [localTags, setLocalTags] = useState<Array<string>>([]);
+
+  const handleChanges = {
+    title: (e: ChangeEvent<HTMLInputElement>) => {
+      const { name, value } = e.target;
+
+      setValues({ ...values, [name]: value });
+
+      if (values.title.length > 30) {
+        setErrors({
+          ...errors,
+          title: '제목의 글자 수는 30자를 넘기지 않아야 합니다.',
+        });
+      }
+
+      if (values.title && values.title.length <= 30) {
+        setErrors({ ...errors, title: '' });
+      }
+    },
+    body: (e: ChangeEvent<HTMLTextAreaElement>) => {
+      const { name, value } = e.target;
+
+      setValues({ ...values, [name]: value });
+
+      if (values.body) {
+        setErrors({ ...errors, body: '' });
+      }
+    },
+    tag: (e: ChangeEvent<HTMLInputElement>) => {
+      const { name, value } = e.target;
+
+      setValues({ ...values, [name]: value });
+
+      values.tag.length > 20
+        ? setErrors({ ...errors, tag: '태그는 20자 이하로 입력해주세요.' })
+        : setErrors({ ...errors, tag: '' });
+    },
+  };
+
+  const handleClicks = {
+    cancelBtn: () => {
+      setValues(initialState.values);
+      history.goBack();
+    },
+    addTagBtn: () => {
+      if (!values.tag) {
+        setErrors({ ...errors, tag: '태그를 입력하세요.' });
+        return;
+      }
+
+      if (values.tag.length > 20) return;
+
+      setLocalTags([...localTags, values.tag]);
+
+      setValues({ ...values, tag: '' });
+    },
+    removeTagBtn: (e, key) => {
+      setLocalTags(prev => prev.filter((_, i) => i !== key));
+    },
+  };
+
+  const handleSubmitPost = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!values.title && !values.body) {
+      setErrors({
+        ...errors,
+        title: '제목을 입력해주세요.',
+        body: '내용을 입력해주세요.',
+      });
+    }
+
+    if (!values.title && values.body) {
+      setErrors({
+        ...errors,
+        title: '제목을 입력해주세요.',
+        body: '',
+      });
+    }
+
+    if (values.title && !values.body) {
+      setErrors({
+        ...errors,
+        title: '',
+        body: '내용을 입력해주세요.',
+      });
+    }
+
+    if (errors.title || errors.body || errors.post) return;
+
+    if (post) {
+      onEdit({ title: values.title, body: values.body, tags: localTags });
+    } else {
+      onWrite({ title: values.title, body: values.body, tags: localTags });
+    }
+  };
+
+  useEffect(() => {
+    if (post) {
+      setValues({ title: post.title, body: post.body, tag: '' });
+      setLocalTags(post.tags);
+    }
+  }, [post]);
+
+  useEffect(() => {
+    if (postErrMsg) {
+      setErrors({ ...errors, post: postErrMsg });
+    }
+  }, [postErrMsg]);
+
+  // useEffect(() => {
+  //   // error
+  //   if (postErrMsg) {
+
+  //     if (values.title && values.body) {
+  //       setErrors({ ...errors, post: '등록에 실패했습니다.' });
+  //     }
+  //   }
+  // }, [postErrMsg, values.body, values.title]);
+
   return (
     <Responsive>
       <StyledAddPostArticle>
@@ -20,9 +160,9 @@ const AddPost = ({
             values={values}
             errors={errors}
             localTags={localTags}
-            onSubmit={onSubmit}
-            onChanges={onChanges}
-            onClicks={onClicks}
+            onSubmit={handleSubmitPost}
+            onChanges={handleChanges}
+            onClicks={handleClicks}
           />
         </section>
         <section className="section postViewer">
@@ -34,4 +174,4 @@ const AddPost = ({
   );
 };
 
-export default AddPost;
+export default withRouter(AddPost);
