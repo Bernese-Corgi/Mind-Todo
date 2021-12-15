049f6e58ffa185c98fe8f3b8c73e80b6e9d0b993
Bernese-Corgi <jnyng0621@gmail.com>
2021-12-15 16:36:15 +0900

    feat: Add checkIsRoot util function in mindmap utils

--- client/src/components/node/NodeDetail/NodeDetail.tsx
+++ client/src/components/node/NodeDetail/NodeDetail.tsx
@@ -9,6 +9,7 @@ import { NodeName } from '..';
 import { TodoUnit } from 'components/todos';
 import { PostViewerContainer } from 'container/posts';
 import { LoadingIcon } from 'components/common';
+import { checkIsRoot } from 'utils/mindmap';
 
 interface NodeDetailProps {
   links: {
@@ -41,13 +42,6 @@ const NodeDetail = ({
   onEdit,
   onRemove,
 }: NodeDetailProps) => {
-  const isRoot = false;
-  const mindmapId = mindmap?._id;
-  // const nodeId = node?._id;
-  // const nodePost = node?.post;
-  // const nodeTodos = node?.todos;
-
-  // console.log(node);
   // if (!node) return <p>aa</p>;
   if (loading) return <LoadingIcon />;
   if (error) return <p>error 발생!</p>;
@@ -60,7 +54,7 @@ const NodeDetail = ({
         <StyledNodeName>
           <NodeName
             nodeName={node?.name}
-            isRoot={isRoot}
+            isRoot={checkIsRoot(mindmap, nodeId)}
             onEdit={onEdit.nodeName}
             onRemove={onRemove.nodeName}
           />
