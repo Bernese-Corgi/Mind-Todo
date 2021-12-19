f7e5b5c0b7cbacb49ad5b94fef31e3908434c763
Bernese-Corgi <jnyng0621@gmail.com>
2021-12-19 22:41:56 +0900

    feat: Add key event to handle toolbar in MdEditor component

--- client/src/components/common/MdEditor/MdEditor.tsx
+++ client/src/components/common/MdEditor/MdEditor.tsx
@@ -1,7 +1,8 @@
-import React, { ChangeEvent } from 'react';
-import { ErrorMsg } from '..';
+import React, { ChangeEvent, KeyboardEvent, RefObject } from 'react';
+import { ErrorMsg, MdToolbar } from '..';
 
 export type MdEditorTextAreaProps = {
+  editorRef?: RefObject<HTMLTextAreaElement>;
   id: string;
   name: string;
   value: string;
@@ -18,6 +19,7 @@ type MdEditorProps = MdEditorTextAreaProps & {
 };
 
 const MdEditor = ({
+  editorRef,
   id = 'mdEditor',
   label = 'Markdown Editor',
   name = 'mdEditor',
@@ -28,23 +30,93 @@ const MdEditor = ({
   readOnly,
   onChange,
   hideLabel = false,
-}: MdEditorProps) => (
-  <div className="mdEditorWrapper">
-    <label className={hideLabel ? 'a11yHidden' : ''} htmlFor={id}>
-      {label && label}
-    </label>
-    <textarea
-      id={id}
-      name={name}
-      value={value}
-      placeholder={placeholder}
-      autoComplete={autoComplete}
-      readOnly={readOnly}
-      onChange={onChange}
-      className={`${errorMsg && 'errorInput'}`}
-    />
-    <ErrorMsg children={errorMsg} />
-  </div>
-);
+}: MdEditorProps) => {
+  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    onChange && onChange(e);
+  };
+
+  const handleKeyPress = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+    if (!editorRef) return;
+    const textVal = editorRef.current?.value;
+    const textLen = textVal?.length;
+    const selectStart = editorRef.current?.selectionStart;
+    const selectEnd = editorRef.current?.selectionEnd;
+
+    if (selectStart === undefined || selectEnd === undefined) return;
+    if (!editorRef.current) return;
+
+    const lastN = textVal?.lastIndexOf('\n', selectStart - 1);
+    const nextN = textVal?.indexOf('\n', selectStart);
+
+    if (lastN && nextN) {
+      const beforeText = textVal?.substring(0, selectStart);
+      const afterText = textVal?.substring(selectStart, textLen);
+
+      const findList = textVal?.substring(lastN, lastN + 3).includes('\n- ');
+
+      if (findList && nextN - lastN === 3 && e.key === 'Enter') {
+        e.preventDefault();
+        if (!beforeText) return;
+        editorRef.current.value =
+          beforeText.substring(0, beforeText.length - 2) + afterText;
+        editorRef.current?.setSelectionRange(
+          beforeText.length - 2,
+          beforeText.length - 2
+        );
+        return;
+      }
+      if (!textLen) return;
+      const lastList = textVal
+        ?.substring(textLen - 3, textLen)
+        .includes('\n- ');
+
+      if (lastList && nextN === -1 && e.key === 'Enter') {
+        e.preventDefault();
+        if (!beforeText) return;
+        editorRef.current.value =
+          beforeText.substring(0, beforeText.length - 2) + afterText;
+        editorRef.current?.setSelectionRange(
+          beforeText.length - 2,
+          beforeText.length - 2
+        );
+        return;
+      }
+
+      if (findList && e.key === 'Enter') {
+        e.preventDefault();
+
+        editorRef.current.value = beforeText + '\n- ' + afterText;
+        if (beforeText) {
+          editorRef.current?.setSelectionRange(
+            selectStart + 3,
+            selectStart + 3
+          );
+        }
+      }
+    }
+  };
+
+  return (
+    <div className="mdEditorWrapper">
+      <label className={hideLabel ? 'a11yHidden' : ''} htmlFor={id}>
+        {label && label}
+      </label>
+      {editorRef && <MdToolbar editorRef={editorRef} />}
+      <textarea
+        ref={editorRef}
+        id={id}
+        name={name}
+        value={value}
+        placeholder={placeholder}
+        autoComplete={autoComplete}
+        readOnly={readOnly}
+        onChange={handleChange}
+        onKeyPress={handleKeyPress}
+        className={`${errorMsg && 'errorInput'}`}
+      />
+      <ErrorMsg children={errorMsg} />
+    </div>
+  );
+};
 
 export default MdEditor;
