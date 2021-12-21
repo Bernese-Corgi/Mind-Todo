b2e2a6a03575c95e0145d4cf88a6480865abddba
Bernese-Corgi <jnyng0621@gmail.com>
2021-12-21 21:32:04 +0900

    style: Modify ToolBox background color

--- client/src/components/common/ToolBox/ToolBox.styled.ts
+++ client/src/components/common/ToolBox/ToolBox.styled.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import theme from 'styles/theme';
 
 export const ToolBoxWrapper = styled.div`
-  background-color: ${theme.colors.gray.light};
+  background-color: ${theme.colors.gray.light}90;
   padding: 0.6em 0.5em 0.2em 0.5em;
   border-radius: ${theme.borders.radius.square};
   position: absolute;
