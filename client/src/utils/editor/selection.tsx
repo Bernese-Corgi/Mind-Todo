d8240608b45c59a5001a8de662f2fdeb31c6ae29
Bernese-Corgi <jnyng0621@gmail.com>
2021-12-23 23:34:12 +0900

    feat: Add function to get textarea state

--- client/src/utils/editor/selection.tsx
+++ client/src/utils/editor/selection.tsx
@@ -5,3 +5,46 @@ export const setSelection = (
   textarea.focus();
   textarea.setSelectionRange(range.start, range.end);
 };
+
+export const getTextareaState = (
+  textarea: HTMLTextAreaElement,
+  location: 'start' | 'current'
+) => {
+  const { value, selectionStart, selectionEnd } = textarea;
+
+  const inputLen = value.length;
+
+  const prevLine = value.lastIndexOf('\n', selectionStart - 1);
+  const nextLine = value.indexOf('\n', selectionStart);
+
+  const commonState = {
+    value,
+    inputLen,
+    prevLine,
+    nextLine,
+  };
+
+  switch (location) {
+    case 'start':
+      return {
+        ...commonState,
+        beforeVal: value.substring(0, prevLine + 1),
+        selectedVal: '',
+        afterVal: value.substring(prevLine + 1, inputLen),
+      };
+    case 'current':
+      return {
+        ...commonState,
+        beforeVal: value.substring(0, selectionStart),
+        selectedVal: value.substring(selectionStart, selectionEnd),
+        afterVal: value.substring(selectionEnd, inputLen),
+      };
+    default:
+      return {
+        ...commonState,
+        beforeVal: value.substring(0, selectionStart),
+        selectedVal: value.substring(selectionStart, selectionEnd),
+        afterVal: value.substring(selectionEnd, inputLen),
+      };
+  }
+};
