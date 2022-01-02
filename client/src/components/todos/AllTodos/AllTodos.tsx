d176e5b1437a5dac58368b64d5070d9ff0a2a543
Bernese-Corgi <jnyng0621@gmail.com>
2022-01-02 12:28:18 +0900

    feat: Add function to filter todo list by node id in AllTodos component

--- client/src/components/todos/AllTodos/AllTodos.tsx
+++ client/src/components/todos/AllTodos/AllTodos.tsx
@@ -1,10 +1,13 @@
+import { useEffect, useState } from 'react';
+import { chunkDateString } from 'utils/stringUtils';
 import { LoadingIcon } from 'components/common';
-import { useState } from 'react';
 import { TodoList } from '..';
 import { AllTodosWrapper } from './AllTodos.styled';
+import { NodeRouteContainer } from 'container/nodes';
+import { TodoListType } from 'utils/api/todos';
 
 interface AllTodosProps {
-  todoList;
+  todoList: TodoListType;
   onToggle: (todoId: string, completed: boolean) => void;
   onEdit: (todoId: string, content: string) => void;
   onDelete: (todoId: string) => void;
@@ -20,27 +23,67 @@ const AllTodos = ({
   loading,
   error,
 }: AllTodosProps) => {
-  // if (loading) return <LoadingIcon />;
+  const [allTodos, setAllTodos] = useState<TodoListType[]>([]);
+
+  const filterTodoListByNode = (allTodoArray: TodoListType) => {
+    const extractIndividualNodeId = allTodoArray.filter((todo, i, todos) => {
+      if (!todo) return;
+
+      return todo?.nodeId!._id !== todos[i + 1]?.nodeId!._id;
+    });
+
+    let arrayOfTodoListSortedByNode: TodoListType[] = [];
+
+    extractIndividualNodeId.map(todo => {
+      let todosWithMatchNode: TodoListType = [];
+
+      // eslint-disable-next-line array-callback-return
+      allTodoArray.map(_todo => {
+        if (todo!.nodeId!._id === _todo!.nodeId!._id) {
+          todosWithMatchNode.push(_todo);
+        }
+      });
+
+      arrayOfTodoListSortedByNode.push(todosWithMatchNode);
+    });
+
+    return arrayOfTodoListSortedByNode;
+  };
+
+  useEffect(() => {
+    if (!todoList) return;
+
+    const filtered = filterTodoListByNode(todoList);
+
+    setAllTodos(filtered);
+  }, [todoList]);
+
   // if (todoList) return <p>no data</p>;
+  // if (loading) return <LoadingIcon />;
   if (error) return <p>error</p>;
 
   return (
     <AllTodosWrapper>
       <h2 className="allTodosTitle">Todo List</h2>
-      <TodoList
-        todos={todoList}
-        onToggle={onToggle}
-        onEdit={onEdit}
-        onDelete={onDelete}
-      />
-      {/* {todoList?.map(todo => {
+      {allTodos.map(todoListByNode => {
+        const node = todoListByNode[0].nodeId;
+        const createdDate =
+          todoListByNode[0].createdAt &&
+          chunkDateString(todoListByNode[0].createdAt);
+
         return (
-          <ul>
-            <time>{chunkDateString(todo.createdAt)}</time>
-            <TodoItem todo={todo} />
-          </ul>
+          <article>
+            {node && <NodeRouteContainer node={node} />}
+            <time>{createdDate}</time>
+            <TodoList
+              todos={todoListByNode}
+              onToggle={onToggle}
+              onEdit={onEdit}
+              onDelete={onDelete}
+            />
+          </article>
         );
-      })} */}
+      })}
     </AllTodosWrapper>
   );
 };
