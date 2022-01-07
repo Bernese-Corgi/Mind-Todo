e302e77aa1be26bd48adc5913edaf2141740cd4c
Bernese-Corgi <jnyng0621@gmail.com>
2022-01-08 07:38:39 +0900

    fix: Correct types of PostViewer

--- client/src/components/posts/PostViewer/PostViewer.tsx
+++ client/src/components/posts/PostViewer/PostViewer.tsx
@@ -12,6 +12,8 @@ import { useEffect, useState } from 'react';
 import { RouteComponentProps, withRouter } from 'react-router';
 import { Link } from 'react-router-dom';
 import { UserType } from 'utils/api/auth';
+import { MindmapType } from 'utils/api/mindmaps';
+import { PostType } from 'utils/api/posts';
 import { getNodeRoute } from 'utils/mindmap';
 import { PostViewerWrapper } from './PostViewer.styled';
 
@@ -22,7 +24,7 @@ interface PostViewerParams {
 }
 
 export interface PostViewerProps {
-  post;
+  post: PostType;
   currentUser?: UserType;
   localTags?: string[];
   hasEdit?: boolean;
@@ -45,23 +47,30 @@ const PostViewer = ({
 }: PostViewerProps & RouteComponentProps<PostViewerParams>) => {
   const { postId } = match.params;
 
-  const isOwnPost =
-    currentUser && post?.publisher.username === currentUser.username;
+  const postMindmapAssert = post?.mindmapId as Partial<MindmapType>;
+  const postPublisherAssert = post?.publisher as UserType;
 
-  const nodeLink = post && `/mindmap/${post.mindmapId._id}/${post.nodeId}`;
+  const mindmapOfPost = postMindmapAssert?.body;
+  const mindmapIdOfPost = postMindmapAssert._id;
+  const postUsername = postPublisherAssert?.username;
+
+  const isOwnPost = currentUser && postUsername === currentUser.username;
+
+  const nodeLink = post && `/mindmap/${mindmapIdOfPost}/${post.nodeId}`;
 
   const [nodeRoute, setNodeRoute] = useState<string>();
 
   useEffect(() => {
-    if (post && post.mindmapId.body) {
-      const matchNode = post.mindmapId.body?.find(
+    if (post && mindmapOfPost) {
+      const matchNode = mindmapOfPost?.find(
         tree => tree.node._id === post.nodeId
       );
 
-      const route = getNodeRoute(matchNode, post.mindmapId.body);
+      const route = matchNode && getNodeRoute(matchNode, mindmapOfPost);
+
       setNodeRoute(route);
     }
-  }, [post]);
+  }, [post, mindmapOfPost]);
 
   if (loading) return <LoadingIcon />;
 
@@ -94,7 +103,7 @@ const PostViewer = ({
           />
         )}
       </div>
-      <SubInfo writer={post.publisher.username} writtenDate={post.createdAt} />
+      <SubInfo writer={postUsername} writtenDate={post.createdAt} />
 
       {hasEdit && isOwnPost && (
         <EditDeleteButtonUnit
