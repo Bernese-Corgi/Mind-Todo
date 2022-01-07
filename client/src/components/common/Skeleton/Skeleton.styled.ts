ebbf601c69a198edbbdb0902e623e0869cba8e6f
Bernese-Corgi <jnyng0621@gmail.com>
2022-01-07 15:25:03 +0900

    style: Add Skeleton animation effect

--- client/src/components/common/Skeleton/Skeleton.styled.ts
+++ client/src/components/common/Skeleton/Skeleton.styled.ts
@@ -47,6 +47,53 @@ const postSkeleton = css`
   height: 7em;
 `;
 
+const animation = css`
+  @keyframes loading {
+    0% {
+      transform: translateX(0);
+    }
+    50%,
+    100% {
+      transform: translateX(460px);
+    }
+  }
+
+  overflow: hidden;
+  position: relative;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 150px;
+    height: 100%;
+
+    background: -moz-linear-gradient(
+      left,
+      rgba(239, 239, 239, 0.1) 0%,
+      rgba(255, 255, 255, 0.15) 40%,
+      rgba(255, 255, 255, 0.15) 60%,
+      rgba(239, 239, 239, 0.1) 100%
+    ); /* FF3.6-15 */
+    background: -webkit-linear-gradient(
+      left,
+      rgba(239, 239, 239, 0.1) 0%,
+      rgba(255, 255, 255, 0.15) 40%,
+      rgba(255, 255, 255, 0.15) 60%,
+      rgba(239, 239, 239, 0.1) 100%
+    ); /* Chrome10-25,Safari5.1-6 */
+    background: linear-gradient(
+      to right,
+      rgba(239, 239, 239, 0.1) 0%,
+      rgba(255, 255, 255, 0.15) 40%,
+      rgba(255, 255, 255, 0.15) 60%,
+      rgba(239, 239, 239, 0.1) 100%
+    ); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+    animation: loading 1.5s infinite linear;
+  }
+`;
+
 export const StyledSkeletonElem = styled.div.attrs<StyledSkeletonElemProps>(
   ({ type }) => ({
     className: type,
@@ -55,6 +102,8 @@ export const StyledSkeletonElem = styled.div.attrs<StyledSkeletonElemProps>(
   background-color: ${theme.colors.gray.light};
   margin: 1em 0;
   border-radius: ${theme.borders.radius.square};
+
+  ${animation}
 
   ${({ type }) => {
     switch (type) {
