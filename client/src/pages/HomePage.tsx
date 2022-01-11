ea8d4a6f7d74a1f128bfa8c163c60156f6e96d99
Bernese-Corgi <jnyng0621@gmail.com>
2022-01-11 23:58:14 +0900

    feat: Add SignButton in HomPage component

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Responsive } from 'components/common';
+import { SignButton } from 'components/auth';
 
 const HomePage = () => {
-  return <Responsive></Responsive>;
+  return (
+    <Responsive>
+      <SignButton />
+    </Responsive>
+  );
 };
 
 export default HomePage;
