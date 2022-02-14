2b835440df8b51e6c0718e2d35a1147893ad30c7
Bernese-Corgi <jnyng0621@gmail.com>
2022-02-14 23:46:44 +0900

    fix: Remove unused code in SignButtonContainer

--- client/src/container/auth/SignButtonContainer.tsx
+++ client/src/container/auth/SignButtonContainer.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import { SignButton } from 'components/auth';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { RootState } from 'redux/modules';
 
-interface SignButtonContainerProps {}
-
-const SignButtonContainer = ({}: SignButtonContainerProps) => {
-  const dispatch = useDispatch();
+const SignButtonContainer = () => {
   const { user } = useSelector(({ user }: RootState) => ({
     user: user.user,
   }));
-
-  console.log(user);
 
   return <>{!user && <SignButton />}</>;
 };
