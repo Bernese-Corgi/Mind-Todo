d99fdd3ebca2fb2f17b4acee5ba9abeeb7b76b92
Bernese-Corgi <jnyng0621@gmail.com>
2022-02-19 01:34:59 +0900

    feat: Add MindmapItem component

--- client/src/components/mindmaps/MindmapList/MindmapList.styld.ts
+++ client/src/components/mindmaps/MindmapList/MindmapList.styld.ts
@@ -3,6 +3,67 @@ import theme from 'styles/theme';
 
 const { fonts } = theme;
 
+export const StyledMindmapItemLi = styled.li`
+  ${({ theme }) => theme.media.desktop`
+    width: 30%;
+   `}
+
+  ${({ theme }) => theme.media.tablet`
+    width: 40%;
+  `}
+
+  ${({ theme }) => theme.media.mobile`
+    width: 80%;
+  `}
+
+
+
+  position: relative;
+  background-color: #fff;
+  border-radius: 2%;
+  margin: 1em;
+  box-shadow: ${theme.boxShadow.default};
+  ${theme.transition()}
+
+  .dim {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    ${theme.transition()}
+  }
+
+  &:hover {
+    transform: translateY(-0.5em);
+
+    .dim {
+      background-color: rgba(0, 0, 0, 0.05);
+    }
+  }
+
+  .link {
+    display: block;
+    width: 100%;
+    height: 100%;
+    padding-bottom: 1em;
+    text-align: left;
+  }
+
+  .title {
+    font-size: 1.2em;
+    padding: 0.5em;
+    margin-top: 0.3em;
+    margin-left: 0.2em;
+    font-weight: ${theme.fonts.weight.bold};
+    padding: 0.3em;
+  }
+
+  ${theme.flexes.mixin('column', 'start', 'start')}
+`;
+
+export const StyledMindmapListUl = styled.ul`
+  ${theme.flexes.mixin('row', 'start', 'start', 'wrap')}
+`;
+
 export const MindmapListWrapper = styled.section`
   font-size: ${theme.fonts.size.sm};
   padding: 2em;
@@ -18,10 +79,6 @@ export const MindmapListWrapper = styled.section`
     margin-left: 80%;
     font-size: 80%;
     width: max-content;
-  }
-
-  .mindmapList {
-    margin: 1em;
   }
 
   ${({ theme }) => theme.media.desktop`
