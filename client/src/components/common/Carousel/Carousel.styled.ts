cc58281add9bdb9b391cb42aa121144e3313617b
Bernese-Corgi <jnyng0621@gmail.com>
2022-02-21 23:39:34 +0900

    style: Add StyledCarouselItem component

---
+++ client/src/components/common/Carousel/Carousel.styled.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import theme from 'styles/theme';
+import { StyledCarouselItemProps } from './Carousel';
+
+export const StyledCarouselItem = styled.img<StyledCarouselItemProps>`
+  opacity: 0;
+  ${theme.positions.absolute}
+  width: 100%;
+  margin: auto;
+  padding: 1rem 4rem;
+  z-index: 100;
+  transition: transform 0.5s, opacity 0.5s, z-index 0.5s;
+
+  transform: ${({ prev }) => (prev ? 'translateX(-100%)' : '')};
+  transform: ${({ next }) => (next ? 'translateX(-100%)' : '')};
+  z-index: ${({ prev, next }) => (prev || next ? 800 : '')};
+
+  ${({ active }) =>
+    active &&
+    `
+      opacity: 1;
+      position: relative;
+      z-index: 100;
+    `}
+`;
