cdc51df145a86b9d2e8fbb1f91c691b64040939c
Bernese-Corgi <jnyng0621@gmail.com>
2022-02-22 23:42:35 +0900

    feat: Add useCarousel component

--- client/src/utils/hooks/index.tsx
+++ client/src/utils/hooks/index.tsx
@@ -2,3 +2,4 @@ export { default as usePrevios } from './usePrevious';
 export { default as useCompare } from './useCompare';
 export { default as useDispatchTodos } from './useDispatchTodos';
 export { default as useS3Bucket } from './useS3Bucket';
+export { default as useCarousel } from './useCarousel';
