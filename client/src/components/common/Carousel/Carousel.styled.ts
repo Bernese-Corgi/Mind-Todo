e5770434103f04ee293c03d872a85cd9b6850971
Bernese-Corgi <jnyng0621@gmail.com>
2022-02-24 23:41:25 +0900

    style: Modify layout in Carousel

--- client/src/components/common/Carousel/Carousel.styled.ts
+++ client/src/components/common/Carousel/Carousel.styled.ts
@@ -3,9 +3,25 @@ import theme from 'styles/theme';
 import { Button } from '..';
 import { CarouselButtonProps, CarouselItemProps } from './Carousel';
 
+const buttonPadding = '3em';
+
 export const StyledCarouselWrapper = styled.div`
+  background-color: tomato;
   width: 100%;
   position: relative;
+  margin: 0 auto;
+  padding: 1em ${buttonPadding};
+
+  ${({ theme }) => theme.media.desktop`
+    width: 30vw;
+  `}
+  ${({ theme }) => theme.media.tablet`
+    width: 40vw;
+  `}
+  ${({ theme }) => theme.media.mobile`
+    width: 80vw;
+  `}
+
   ${theme.flexes.row('center')}
 `;
 
@@ -14,7 +30,9 @@ interface StyledCarouselProps {
 }
 
 export const StyledCarousel = styled.div<StyledCarouselProps>`
-  width: 90%;
+  width: 100%;
+  overflow: hidden;
+  background-color: gold;
 `;
 
 interface StyledCarouselUlProps {
@@ -23,16 +41,15 @@ interface StyledCarouselUlProps {
 }
 
 export const StyledCarouselUl = styled.ul<StyledCarouselUlProps>`
-  overflow: hidden;
   ${theme.flexes.row('center')}
 
-  transition: transform ${({ duration }) => duration};
-  transform: translate3d(calc(var(--currentSlide) * 100%), 0, 0);
-  transform: translate3d(${({ currentSlide }) => currentSlide * -100}%, 0);
+  transition: transform ${({ duration }) => duration}ms ease-out;
+  /* transform: translate3D(calc(var(--currentSlide) * -100%), 0, 0); */
+  transform: translate3D(${({ currentSlide }) => currentSlide * -100}%, 0, 0);
 `;
 
 export const StyledCarouselItem = styled.li<CarouselItemProps>`
-  width: 300px;
+  /* width: 500px; */
   margin: 1em;
 `;
 
@@ -46,6 +63,8 @@ export const StyledCarouselButton = styled(Button).attrs<CarouselButtonProps>(
   top: 50%;
   transform: translateY(-50%) rotate(90deg);
   z-index: 99;
+  background-color: blue;
+  padding: calc(${buttonPadding} / 6);
 
   ${({ direction }) =>
     direction === 'prev'
