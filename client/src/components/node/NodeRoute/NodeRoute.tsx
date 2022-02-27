444bbde0c16e4e4c06252674e888cc174b0d4bcd
Bernese-Corgi <jnyng0621@gmail.com>
2022-02-27 17:07:22 +0900

    fix: Modify style of NodeRoute to highlight only node name

--- client/src/components/node/NodeRoute/NodeRoute.tsx
+++ client/src/components/node/NodeRoute/NodeRoute.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Heading } from 'components/common';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import theme from 'styles/theme';
 
 interface NodeRouteProps {
   level?: number;
@@ -9,10 +11,44 @@ interface NodeRouteProps {
   className?: string;
 }
 
+const StyledNodeRouteLink = styled(Link)`
+  .route {
+    font-size: 0.85em;
+    color: ${theme.colors.gray.base};
+  }
+`;
+
+const NodeRouteContent = ({ content }) => {
+  const lastIndex = content.lastIndexOf('>');
+  const route = content.substring(0, lastIndex + 2);
+  const nodeName = content.substring(lastIndex + 2, content.length);
+
+  return (
+    <>
+      {lastIndex === -1 ? (
+        <p>{content}</p>
+      ) : (
+        <>
+          <span className="route">{route}</span>
+          <span className="name">{nodeName}</span>
+        </>
+      )}
+    </>
+  );
+};
+
 const NodeRoute = ({ level = 3, link, content, className }: NodeRouteProps) => {
   return (
     <Heading level={level} className={className}>
-      {link ? <Link to={link} children={content} /> : content}
+      <>
+        {link ? (
+          <StyledNodeRouteLink to={link}>
+            <NodeRouteContent content={content} />
+          </StyledNodeRouteLink>
+        ) : (
+          <NodeRouteContent content={content} />
+        )}
+      </>
     </Heading>
   );
 };
