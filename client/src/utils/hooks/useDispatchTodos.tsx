65a02ad98fdccce0eb258ad9474d4b976c0c0948
Bernese-Corgi <jnyng0621@gmail.com>
2022-04-03 23:55:04 +0900

    feat: Add ToddoDataHandlerType

--- client/src/utils/hooks/useDispatchTodos.tsx
+++ client/src/utils/hooks/useDispatchTodos.tsx
@@ -3,6 +3,16 @@ import { useDispatch } from 'react-redux';
 import { removeTodoAsync, updateTodoAsync } from 'redux/modules/todos/todo';
 import { listTodosAsync } from 'redux/modules/todos/todos';
 
+export type TodoDataHandlerType = {
+  onToggle: (todoId: string, completed: boolean) => void;
+  onEdit: (todoId: string, content: string) => void;
+  onDelete: (todoId: string) => void;
+};
+
+/**
+ * todo의 토글, 수정, 삭제를 위한 thunk 비동기 액션을 수행하는 함수들을 반환하는 커스텀 훅
+ * @returns { handleToggle, handleEdit, handleDelete }
+ */
 const useDispatchTodos = () => {
   const dispatch = useDispatch();
 
